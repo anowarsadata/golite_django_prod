45aadc420ed6a3de48431b2182d36cc54b4bbcac
Anowar Sadat <anowar.vista@gmail.com>
2026-01-09 17:21:12 +0530

    login update

--- core/settings.py
+++ core/settings.py
@@ -196,4 +196,21 @@ USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
+# ===============================
+# EMAIL / SMTP CONFIGURATION
+# ===============================
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtpout.secureserver.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "info@zoikogroup.com"
+EMAIL_HOST_PASSWORD = "NoxxMC26070%!LGM"
+
+DEFAULT_FROM_EMAIL = "Zoiko Group <info@zoikogroup.com>"
+
+
+
 # settings.py cors
