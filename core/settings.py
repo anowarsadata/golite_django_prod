95daabb76c707be7644aa711977c287b1949cfce
Anowar Sadat <anowar.vista@gmail.com>
2026-01-29 18:46:43 +0530

    url add

--- core/settings.py
+++ core/settings.py
@@ -176,6 +176,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5173",
     "https://golitereact.vercel.app",
     "https://react.golitemobile.com",
     "https://lakhan-golite.vercel.app",
