83517f5e7a87b5ee4798c61c3260ff9b4b6d350b
Anowar Sadat <anowar.vista@gmail.com>
2026-02-09 15:04:10 +0530

    coupon

--- apps/coupons/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from apps.coupons.views import apply_coupon, preview_coupon
-
-urlpatterns = [
-    path("apply-coupon/", apply_coupon, name="apply_coupon"),
-    path("preview-coupon/", preview_coupon, name="preview_coupon"),
-]
